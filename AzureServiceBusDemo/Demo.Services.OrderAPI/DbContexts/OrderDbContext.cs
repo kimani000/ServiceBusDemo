9aa0f43d3d961df251ad12538c9a7667a513542a
Javon Spencer <hoop.spencer@gmail.com>
2023-07-27 17:53:10 -0700

    -figrued out how to do a migration and added a new database for orders

--- AzureServiceBusDemo/Demo.Services.OrderAPI/DbContexts/OrderDbContext.cs
+++ AzureServiceBusDemo/Demo.Services.OrderAPI/DbContexts/OrderDbContext.cs
@@ -5,8 +5,7 @@ namespace Demo.Services.OrderAPI.DbContexts
 {
     public class OrderDbContext : DbContext
     {
-        public OrderDbContext(DbContextOptions<OrderDbContext> options)
-        { }
+        public OrderDbContext(DbContextOptions<OrderDbContext> options) : base(options) { }
 
         public virtual DbSet<OrderDetails> OrderDetails { get; set; }
         public virtual DbSet<OrderHeader> OrderHeader { get; set; }
