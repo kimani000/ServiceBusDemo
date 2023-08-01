131649ae4f6539f7e10b09695da6801f618c681f
kimaniavery075@gmail.com <kimaniavery075@gmail.com>
2023-07-31 22:07:06 -0700

    - Created a new API project for ShoppingCart with basic configurations. - Created models, dbcontext, entities, and completed initial migration.

---
+++ AzureServiceBusDemo/Demo.Services.ShoppingCartAPI/Models/ShoppingCart.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Demo.Services.ShoppingCartAPI.Models
+{
+    [Table(nameof(ShoppingCart))]
+    public class ShoppingCart
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; }
+
+        public virtual ICollection<ShoppingCartDetail> ShoppingCartDetail { get; set; }
+    }
+}
---
+++ AzureServiceBusDemo/Demo.Services.ShoppingCartAPI/Models/ShoppingCartDetail.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Demo.Services.ShoppingCartAPI.Models
+{
+    [Table(nameof(ShoppingCartDetail))]
+    public class ShoppingCartDetail
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid ShoppingCartDetailId { get; set; }
+
+        public Guid ShoppingCartId { get; set; }
+
+        [ForeignKey(nameof(ShoppingCartId))]
+        public ShoppingCart ShoppingCart { get; set; }
+
+        public Guid ProductId { get; set; }
+
+        public Product Product { get; set; }
+        public int Count { get; set; }
+    }
+}
