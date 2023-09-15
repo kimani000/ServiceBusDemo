00074eef9ea42e6a4304cb29216875632ef30032
kimaniavery075@gmail.com <kimaniavery075@gmail.com>
2023-09-14 20:59:49 -0700

    Implemented Interface/Service methods.

--- AzureServiceBusDemo/Demo.Services.AccountAPI/Interfaces/IAccountService.cs
+++ AzureServiceBusDemo/Demo.Services.AccountAPI/Interfaces/IAccountService.cs
@@ -1,6 +1,28 @@
-﻿namespace Demo.Services.AccountAPI.Interfaces
+﻿using Demo.Services.AccountAPI.Models;
+
+namespace Demo.Services.AccountAPI.Interfaces
 {
     public interface IAccountService
     {
+        /// <summary>
+        /// Get an account by id
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <returns></returns>
+        Task<Account> GetAccountById(Guid userId);
+
+        /// <summary>
+        /// Delete an account
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <returns></returns>
+        Task<bool> DeleteAccount(Guid userId);
+
+        /// <summary>
+        /// Patch an account
+        /// </summary>
+        /// <param name="account"></param>
+        /// <returns></returns>
+        Task<Account> PatchAccount(Account account);
     }
 }
--- AzureServiceBusDemo/Demo.Services.AccountAPI/Services/AccountService.cs
+++ AzureServiceBusDemo/Demo.Services.AccountAPI/Services/AccountService.cs
@@ -1,8 +1,53 @@
-﻿using Demo.Services.AccountAPI.Interfaces;
+﻿using Demo.Services.AccountAPI.DbContexts;
+using Demo.Services.AccountAPI.Interfaces;
+using Demo.Services.AccountAPI.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Demo.Services.AccountAPI.Services
 {
     public class AccountService : IAccountService
     {
+        private readonly AccountDbContext _context;
+
+        public AccountService(AccountDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Account> GetAccountById(Guid userId)
+        {
+            return await _context.Accounts.FirstOrDefaultAsync(x => x.Id == userId);
+        }
+
+        public async Task<bool> DeleteAccount(Guid userId)
+        {
+            var account = _context.Accounts.FirstOrDefault(x => x.Id == userId);
+
+            if (account == null)
+            {
+                return false;
+            }
+
+            _context.Remove(account);
+            await _context.SaveChangesAsync();
+
+            return true;
+        }
+
+
+        public async Task<Account> PatchAccount(Account account)
+        {
+            var accountExists = _context.Accounts.Any(x => x.Id == account.Id);
+
+            if (!accountExists)
+            {
+                return null;
+            }
+
+            _context.Entry(account).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+
+            return account;
+        }
     }
 }
